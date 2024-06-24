286b78f7c50af688fd50792b56f75af03170904e
Ester Chang <ester@squareup.com>
2024-06-24 14:14:13 -0700

    embed css in source instead of using external cdn

---
+++ config/application.rb
@@ -0,0 +1,25 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RakeUI
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Add additional assets to the asset load path
+    config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets')
+  end
+end
